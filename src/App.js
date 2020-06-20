b046d6cc35b3d06c23d42666d02e37a4383e9f7a
Bharat-Mandava <43178562+Bharat-Mandava@users.noreply.github.com>
2020-06-20 14:10:36 -0500

    Updated Title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
       <div className="ml-6 pt-1">
         <h1 className="text-2xl text-blue-700 leading-tight">
-          Tailwind and Create React App
+          Tailwind and Create React App - June. 20. 2020
         </h1>
         <p className="text-base text-gray-700 leading-normal">
           Building apps together
